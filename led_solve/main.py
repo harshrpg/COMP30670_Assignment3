56344cb045ec2bb2fb48482ffb95754a25d16fb2
Harsh Gupta <harshrp94@gmail.com>
2018-02-28 18:10:12 +0000

    added functionality to accept input either as a local file or a network file

--- led_solve/main.py
+++ led_solve/main.py
@@ -1,16 +1,25 @@
 import argparse
 from Lights import Lights
 import re
+import requests
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", help="Parameter required to provide input to the program", action="store_true")
+    parser.add_argument(
+        "--input", "-i", help="Parameter required to provide input to the program", action="store_true")
     parser.add_argument("filename", help="Complete path for the file")
     args = parser.parse_args()
+    filePath = args.filename
     if args.input:
         count = ""
-        with open(args.filename, 'r') as fin:
+        if filePath.startswith("http:"):
+            response = requests.get(filePath)
+            with open('input.txt', 'w') as fout:
+                fout.writelines(response.text)
+            fout.close()
+            filePath = 'input.txt'
+        with open(filePath) as fin:
             L = int(fin.readline())
             if L > 0:
                 _lights = Lights(L)
@@ -20,15 +29,16 @@ def main():
                 result = regex.search(i)
                 if result != None:
                     method = str(result.group(1))
+                    # print(method)
                     l1 = int(result.group(2))
                     l2 = int(result.group(3))
                     l3 = int(result.group(4))
                     l4 = int(result.group(5))
                     if l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l1 < L and l2 < L and l3 < L and l4 < L:
                         count = _lights.runCmd(method, l1, l2, l3, l4)
+                        # print('Success')
                     else:
                         continue
-
         print(count)
 
 
