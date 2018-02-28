47c51928bd32e221f676b8f7d675a4848b3ba316
Harsh Gupta <harshrp94@gmail.com>
2018-02-28 14:39:44 +0000

    Completeing the class and testing with an input file

--- led_solve/Lights.py
+++ led_solve/Lights.py
@@ -6,3 +6,34 @@ class Lights(object):
         self.__grid =  [[False]*self.__L for i in range(self.__L)]
         self.__onCount = 0
         self.__offCount = L*L
+
+    def runCmd(self, method, l1, l2, l3, l4):
+        """Toggle the state of lights"""
+        if method == "turn on":
+            for r in range(l1,l3+1):
+                for c in range(l2,l4+1):
+                    if self.__grid[r][c]==False:
+                        self.__grid[r][c]=True
+                        self.__onCount+=1
+                        self.__offCount-=1
+
+        elif method == "turn off":
+            for r in range(l1,l3+1):
+                for c in range(l2,l4+1):
+                    if self.__grid[r][c] == True:
+                        self.__grid[r][c]=False
+                        self.__offCount+=1
+                        self.__onCount-=1
+
+        elif method == "switch":
+            for r in range(l1,l3+1):
+                for c in range(l2,l4+1):
+                    if self.__grid[r][c] == True:
+                        self.__grid[r][c]=False
+                        self.__offCount+=1
+                        self.__onCount-=1
+                    else:
+                        self.__grid[r][c]=False
+                        self.__onCount+=1
+                        self.__offCount-=1
+        return(self.__onCount,self.__offCount)
--- led_solve/main.py
+++ led_solve/main.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
-"""Main module."""
-import re
 from Lights import Lights
+import re
 def main():
     count = ""
     with open('input.in') as fin:
         L = int(fin.readline())
-        if L > 0:
+        if L>0:
             _lights = Lights(L)
         regex = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
         for i in fin.readlines():
@@ -18,8 +15,11 @@ def main():
                 l2 = int(result.group(3))
                 l3 = int(result.group(4))
                 l4 = int(result.group(5))
-                print(method,l1,l2,l3,l4)
+                if l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l1 < L and l2 < L and l3 < L and l4 < L:
+                    count = _lights.runCmd(method,l1,l2,l3,l4)
+                else:
+                    continue
+    print(count)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
