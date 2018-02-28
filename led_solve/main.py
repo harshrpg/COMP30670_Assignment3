24bf4ca09e9db07c16d07821c2349f5083f38d9d
Harsh Gupta <harshrp94@gmail.com>
2018-02-28 15:53:34 +0000

    Including argument parsing capabilities to the code

--- led_solve/main.py
+++ led_solve/main.py
@@ -1,25 +1,36 @@
+import argparse
 from Lights import Lights
 import re
-def main():
-    count = ""
-    with open('input.in') as fin:
-        L = int(fin.readline())
-        if L>0:
-            _lights = Lights(L)
-        regex = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
-        for i in fin.readlines():
-            result = regex.search(i)
-            if result != None:
-                method = str(result.group(1))
-                l1 = int(result.group(2))
-                l2 = int(result.group(3))
-                l3 = int(result.group(4))
-                l4 = int(result.group(5))
-                if l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l1 < L and l2 < L and l3 < L and l4 < L:
-                    count = _lights.runCmd(method,l1,l2,l3,l4)
-                else:
-                    continue
-    print(count)
 
-if __name__=="__main__":
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", help="Parameter required to provide input to the program", action="store_true")
+    parser.add_argument("filename", help="Complete path for the file")
+    args = parser.parse_args()
+    if args.input:
+        count = ""
+        with open(args.filename, 'r') as fin:
+            L = int(fin.readline())
+            if L > 0:
+                _lights = Lights(L)
+            regex = re.compile(
+                ".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+            for i in fin.readlines():
+                result = regex.search(i)
+                if result != None:
+                    method = str(result.group(1))
+                    l1 = int(result.group(2))
+                    l2 = int(result.group(3))
+                    l3 = int(result.group(4))
+                    l4 = int(result.group(5))
+                    if l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l1 < L and l2 < L and l3 < L and l4 < L:
+                        count = _lights.runCmd(method, l1, l2, l3, l4)
+                    else:
+                        continue
+
+        print(count)
+
+
+if __name__ == "__main__":
     main()
