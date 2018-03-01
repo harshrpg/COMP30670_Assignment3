814d5b9187b0820d7ae4dd8be28fc8b021f94544
Harsh Gupta <harshrp94@gmail.com>
2018-03-01 15:00:16 +0000

    Changing the loop structure for the Lights class

--- led_solve/Lights.py
+++ led_solve/Lights.py
@@ -3,38 +3,44 @@ class Lights(object):
     def __init__(self, L):
         """Constructor for the class"""
         self.__L = L
-        self.__grid =  [[False]*self.__L for i in range(self.__L)]
+        self.__grid = [[False]*self.__L for i in range(self.__L)]
         self.__onCount = 0
         self.__offCount = L*L
 
     def runCmd(self, method, l1, l2, l3, l4):
+        # print('Inside runcms')
         """Toggle the state of lights"""
         if method == "turn on":
-            for r in range(l2, l4+1):
-                for c in range(l1, l3+1):
-                    if self.__grid[r][c]==False:
-                        self.__grid[r][c]=True
-                        self.__onCount+=1
-                        self.__offCount-=1
+            if l1 <= l3 and l2 <= l4:
+                for row in range(l2, l4+1):
+                    # print(l4)
+                    # print(row)
+                    for i, r in enumerate(self.__grid[row]):
+                        if i >= l1 and i <= l3 and self.__grid[row][i] == False:
+                            self.__grid[row][i] = True
+
         elif method == "turn off":
-            for r in range(l2, l4+1):
-                for c in range(l1, l3+1):
-                    if self.__grid[r][c] == True :
-                        self.__grid[r][c]=False
-                        self.__offCount+=1
-                        self.__onCount-=1
+            if l1 <= l3 and l2 <= l4:
+                for row in range(l2, l4+1):
+                    # print(l4)
+                    # print(row)
+                    for i, r in enumerate(self.__grid[row]):
+                        if i >= l1 and i <= l3 and self.__grid[row][i] == True:
+                            self.__grid[row][i] = False
 
         elif method == "switch":
-            print(l1,l2,l3,l4)
-            for r in range(l2,l4+1):
-                for c in range(l1,l3+1):
-                    if self.__grid[r][c] == False:
-                        self.__grid[r][c] = True
-                        self.__onCount += 1
-                        self.__offCount -= 1
-                    elif self.__grid[r][c] == True:
-                        self.__grid[r][c] = False
-                        self.__offCount +=1
-                        self.__onCount -= 1
+            if l1 <= l3 and l2 <= l4:
+                for row in range(l2, l4+1):
+                    # print(l4)
+                    # print(row)
+                    for i, r in enumerate(self.__grid[row]):
+                        if i >= l1 and i <= l3:
+                            if self.__grid[row][i]:
+                                self.__grid[row][i] = False
+                            else:
+                                self.__grid[row][i] = True
 
-        return("Leds ON: {}, Leds OFF: {}".format(self.__onCount,self.__offCount))
+    def counts(self):
+        self.__onCount = (sum([row.count(True) for row in self.__grid]))
+        self.__offCount = self.__offCount-self.__onCount
+        return(self.__onCount, self.__offCount)
--- led_solve/solve_led.py
+++ led_solve/solve_led.py
@@ -1,11 +1,10 @@
 import argparse
-from led_solve import Lights
+from Lights import Lights
 import re
-import os
 import requests
 
 
-def solve_led():
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input", "-i", help="Parameter required to provide input to the program", action="store_true")
@@ -16,10 +15,6 @@ def solve_led():
         count = ""
         if filePath.startswith("http:"):
             response = requests.get(filePath)
-            try:
-                os.remove('input.txt')
-            except OSError:
-                pass
             with open('input.txt', 'w') as fout:
                 fout.writelines(response.text)
             fout.close()
@@ -27,29 +22,31 @@ def solve_led():
         with open(filePath) as fin:
             L = int(fin.readline())
             if L > 0:
-                _lights = Lights.Lights(L)
+                _lights = Lights(L)
             regex = re.compile(
                 ".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
             for i in fin.readlines():
                 result = regex.search(i)
                 if result != None:
                     method = str(result.group(1))
-                    # print(method)
+                    # print(method.strip())
                     l1 = int(result.group(2))
                     l2 = int(result.group(3))
                     l3 = int(result.group(4))
                     l4 = int(result.group(5))
-                    if l1 >= 0 and l2 >= 0 and l3 >= 0 and l4 >= 0 and l1 < L and l2 < L and l3 < L and l4 < L:
-                        count = _lights.runCmd(method, l1, l2, l3, l4)
-                        # print('Success')
-                    else:
-                        continue
+                    if l1 < 0:
+                        l1 = 0
+                    if l3 < 0:
+                        l3 = 0
+                    if l2 > L:
+                        l2 = L-1
+                    if l4 > L:
+                        l4 = L-1
+                    _lights.runCmd(method, l1, l2, l3, l4)
+
+        count = _lights.counts()
         print(count)
-        try:
-            os.remove('input.txt')
-        except OSError:
-            pass
 
 
 if __name__ == "__main__":
-    solve_led()
+    main()
